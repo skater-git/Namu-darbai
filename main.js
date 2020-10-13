c204561897a7d17dc5c6406d8bac4312c82d840e
skater-git <72792717+skater-git@users.noreply.github.com>
2020-10-13 13:49:52 +0300

    javascript pradzia

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log('labas');
+console.log("ate");
+
+var tekstasVienas = 'kazkoks tai tekstas';
+var sveikasSkaicius = '2';
+var skaicius = '2.567';
+var tiesa = true;
+
+console.log(tekstasVienas, skaicius);
+console.log(sveikasSkaicius);
+console.log(true);
+console.log(sveikasSkaicius + skaicius);
+console.log(tekstasVienas + ' ' + skaicius);
